a2ce89ff8821878f9e8089b9a61ed6e7f2f522e8
Ben (X13/Arch) <ben@wolkenwelten.net>
2024-03-22 21:02:06 +0100

    Bounce baby

--- src/scenes/game/Bullet.ts
+++ src/scenes/game/Bullet.ts
@@ -15,5 +15,12 @@ export class Bullet extends Physics.Arcade.Image {
     preUpdate(time: number, delta: number) {
         this.x += this.vx * delta;
         this.y += this.vy * delta;
+
+        if((this.y > 720) || (this.y < 0)){
+            this.vy *= -1;
+        }
+        if((this.x > 1280) || (this.x < 0)){
+            this.vx *= -1;
+        }
     }
 }
